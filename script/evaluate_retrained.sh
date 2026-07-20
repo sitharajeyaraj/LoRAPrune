177b9d8c8e97b10c9559ab76425bb2b3137c1ed0
sitharajeyaraj <sitharajeyaraj@gmail.com>
2026-07-20 17:41:30 +0530

    Fix evaluate_retrained.sh: pass cutoff_len 2048 to match evaluate.sh convention

--- script/evaluate_retrained.sh
+++ script/evaluate_retrained.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python evaluate_retrained.py \
     --pruned_dir 'outputs_oneshot_samples_20000_ratio_0.1_pruned' \
-    --adapter_dir 'outputs_oneshot_samples_20000_ratio_0.1_retrained'
-    
+    --adapter_dir 'outputs_oneshot_samples_20000_ratio_0.1_retrained' \
+    --cutoff_len 2048
